5b62e968acfeffc9aa1c63073314bf12fb64cf8f
Abdul Rahim <32771684+rahimkhanabdul@users.noreply.github.com>
2026-04-14 20:14:34 +0400

    improvements

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ $(window).on('load',function(){
 
 $(window).on('scroll',function(){
   var scroll = $(window).scrollTop();
-  console.log(scroll);
   if(scroll >=50){
     $(".sticky").addClass("stickyadd");
   }else{
@@ -22,12 +21,12 @@ var waypoint = new Waypoint({
   handler: function() {
 
     var p = document.querySelectorAll('.progress-bar');
-    p[0].setAttribute("style", "width:100%;transition:1s all;");
-    p[1].setAttribute("style", "width:95%;transition:1.5s all;");
+    p[0].setAttribute("style", "width:95%;transition:1s all;");
+    p[1].setAttribute("style", "width:90%;transition:1.5s all;");
     p[2].setAttribute("style", "width:85%;transition:1.7s all;");
-    p[3].setAttribute("style", "width:99%;transition:2s all;");
+    p[3].setAttribute("style", "width:80%;transition:2s all;");
     p[4].setAttribute("style", "width:85%;transition:2.3s all;");
-    p[5].setAttribute("style", "width:95%;transition:2.5s all;");
+    p[5].setAttribute("style", "width:80%;transition:2.5s all;");
 
 
   },
@@ -88,7 +87,7 @@ var filterizd = $('.filter-container').filterizr({
 
 
   var typed = new Typed(".element", {
-  strings: ["Abdul Rahim", "a Developer","a DevOps Enthusiast"],
+  strings: ["Abdul Rahim", "a Solution Architect", "a Senior Backend Engineer"],
   smartBackspace: true,
    typeSpeed: 100,
    backSpeed: 100,
